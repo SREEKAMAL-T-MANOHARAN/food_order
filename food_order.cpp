e892958d5bd7ec576eff3e33081fd7c1da91db9a
SREEKAMAL-T-MANOHARAN <91663128+SREEKAMAL-T-MANOHARAN@users.noreply.github.com>
2021-10-16 08:18:40 +0530

    Add files via upload

---
+++ food_order.cpp
@@ -0,0 +1,26 @@
+#include <iostream.h>
+using namespace std;
+int main()
+{
+    {
+	int curry;
+	string name,food;
+	cout <<"  Please Enter Your Name" << endl;
+	cin >> name;
+	cout <<"  Please Oder Your Food:1) chikan biriyani\n\t\t\t2) masaladoshan\n\t\t\t3) poroota\n\t\t\t4) appam\n\t\t\t5) puttu" << endl;
+	cin >> food;
+//	cout<<"  Please Oder Your Curry:1) kadala curry\n\t\t\t 2)motta curry\n\t\t\t 3)chiken curry\n\t\t\t"<< endl;
+//	cin >> curry;
+	cout <<"Name;"<<name<<endl;
+	cout <<"Food;"<<food<<endl;
+//	cout <<"Curry;"<<curry<<endl;
+    }
+   { 
+#include <iostream>
+using namespace std;
+
+	cout<<"YOUR ORDER WILL BE READY IN A FEW MIN ";
+	
+	return 0;
+   }
+}
